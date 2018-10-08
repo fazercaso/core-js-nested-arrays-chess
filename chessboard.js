b26d073123ccc7c59e22fe49711fad8153d2c5a3
georgebabayan <ggbabayan@gmail.com>
2018-10-08 15:34:54 +0300

    Adds tests

--- chessboard.js
+++ chessboard.js
@@ -1,12 +1,12 @@
 function generate_chessboard() {
 	return [
-		['Б Ладья', 'Б Конь', 'Б Ферзь', 'Б Королева', 'Б Король', 'Б Ферзь', 'Б Конь', 'Б Ладья'],
+		['Ч Ладья', 'Ч Конь', 'Ч Ферзь', 'Ч Королева', 'Ч Король', 'Ч Ферзь', 'Ч Конь', 'Ч Ладья'],
+		['Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка' , 'Ч Пешка'],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
 		['Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка' , 'Б Пешка'],
-		[null, null, null, null, null, null, null, null],
-		[null, null, null, null, null, null, null, null],
-		[null, null, null, null, null, null, null, null],
-		[null, null, null, null, null, null, null, null],
-		['W Пешка', 'W Пешка', 'W Пешка', 'W Пешка', 'W Пешка', 'W Пешка', 'W Пешка' , 'W Пешка'],
-		['W Ладья', 'W Конь', 'W Ферзь', 'W Королева', 'W Король', 'W Ферзь', 'W Конь', 'W Ладья']
+		['Б Ладья', 'Б Конь', 'Б Ферзь', 'Б Королева', 'Б Король', 'Б Ферзь', 'Б Конь', 'Б Ладья']
 	]
-	}
+}
--- spec/chessboard_spec.js
+++ spec/chessboard_spec.js
@@ -1,40 +1,24 @@
-require_relative '../chessboard'
+describe("Board", function() {
 
-describe 'generate_chessboard' do
-  let(:board) { generate_chessboard }
+  beforeEach(function() {
+    board = generate_chessboard();
+  });
 
-  describe 'board structure' do
-    it 'has eight rows' do
-      expect(board.length).to eq 8
-    end
+  describe("#placePiece", function() {
+    it("should return the correct piece for a given starting position", function() {
+      expect(board[0][0]).toEqual('Ч Ладья');
+      expect(board[1][1]).toEqual('Ч Пешка');
+      expect(board[7][7]).toEqual('Б Ладья');
+      expect(board[5][4]).toEqual(null);
+    });
+  });
 
-    it 'has rows with eight spaces' do
-      row_lengths = board.map(&:length)
-
-      expect(row_lengths).to all(be 8)
-    end
-  end
-
-  describe 'piece placement' do
-    it 'has a first row of black backline pieces' do
-      expect(board.first).to eq ['B Rook', 'B Knight', 'B Bishop', 'B Queen', 'B King', 'B Bishop', 'B Knight', 'B Rook']
-    end
-
-    it 'has a last row of while backline pieces' do
-      expect(board.last).to eq ['W Rook', 'W Knight', 'W Bishop', 'W Queen', 'W King', 'W Bishop', 'W Knight', 'W Rook']
-    end
-
-    it 'has a second row of all black pawns' do
-      expect(board[1]).to eq Array.new(8) { 'B Pawn' }
-    end
-
-    it 'has a second to last row of all white pawns' do
-      expect(board[-2]).to eq Array.new(8) { 'W Pawn' }
-    end
-
-    it 'has four empty rows' do
-      empty_rows_count = board.count { |row| row.all?(&:nil?) }
-      expect(empty_rows_count).to eq 4
-    end
-  end
-end
+  describe("#init", function() {
+    it("should generate an 8x8 array", function() {
+      expect(board.length).toEqual(8);
+      for (let i = 0; i < board.length; i++) {
+        expect(board[i].length).toEqual(8);
+      }
+    });
+  });
+});
