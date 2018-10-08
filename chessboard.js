9ccde052580a7a52e34faf983645b98dfdd5530d
Даниил Капустин <daniilkapustin@MacBook-Air.local>
2018-10-08 15:14:08 +0300

    Modifies readme, js files

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,12 @@
 function generateChessboard() {
-
-}
+	return [
+		['Б Ладья', 'Б Конь', 'Б Ферзь', 'Б Королева', 'Б Король', 'Б Ферзь', 'Б Конь', 'Б Ладья'],
+		['Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка' , 'Б Пешка'],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		['Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка', 'Ч Пешка' , 'Ч Пешка'],
+		['Ч Ладья', 'Ч Конь', 'Ч Ферзь', 'Ч Королева', 'Ч Король', 'Ч Ферзь', 'Ч Конь', 'Ч Ладья']
+	]
+	}
