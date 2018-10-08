bb6e1ce2232fc880d3d1f9c3776ddb3255e162f9
George Babayan <ggbabayan@gmail.com>
2018-10-08 14:08:40 +0300

    Update and rename chessboard.rb to chessboard.js

---
+++ chessboard.js
@@ -0,0 +1,12 @@
+function generate_chessboard() {
+	return [
+		['Б Ладья', 'Б Конь', 'Б Ферзь', 'Б Королева', 'Б Король', 'Б Ферзь', 'Б Конь', 'Б Ладья'],
+		['Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка', 'Б Пешка' , 'Б Пешка'],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		['W Пешка', 'W Пешка', 'W Пешка', 'W Пешка', 'W Пешка', 'W Пешка', 'W Пешка' , 'W Пешка'],
+		['W Ладья', 'W Конь', 'W Ферзь', 'W Королева', 'W Король', 'W Ферзь', 'W Конь', 'W Ладья']
+	]
+	}
