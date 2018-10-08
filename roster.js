de489a2d5039ab80f4820bb1a689584c0953d416
Даниил Капустин <daniilkapustin@MacBook-Air.local>
2018-10-08 15:44:08 +0300

    Changes tasks

--- roster.js
+++ roster.js
@@ -1,10 +1,14 @@
+const numbers = [12, 9, 31, 18, 22]
+const names = ['Joe Schmo', 'Ms. Buckets', 'Harvey Kay', 'Sally Talls', 'MK DiBoux']
+const positions = ['Center', 'Point Guard', 'Shooting Guard', 'Power Forward', 'Small Forward']
+const ponits = {
+	12: [14, 32, 7, 0, 23],
+	9: [19, 0, 11, 22, 0],
+	31: [0, 30, 16, 0, 25],
+	18: [18, 29, 26, 31, 19],
+	22: [11, 0, 23, 17, 0]
+}
+
 function generateRoster() {
- return [
-    ['Number', 'Name', 'Position', 'Points per Game'],
-    [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]],
-    [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0]],
-    [31, 'Harvey Kay', 'Shooting Guard',  [0, 30, 16, 0, 25]],
-    [18, 'Sally Talls', 'Power Forward', [18, 29, 26, 31, 19]],
-    [22, 'MK DiBoux', 'Small Forward', [11, 0, 23, 17, 0]],
-  ]
+// Код тут.
 }
