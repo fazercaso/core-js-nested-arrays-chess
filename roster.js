f0009d2f92da1d80a250cd9d187624d01ebfbfc3
kayday <kadaniilo@gmail.com>
2019-04-17 10:46:26 +0300

    points fixed

--- roster.js
+++ roster.js
@@ -1,7 +1,7 @@
 const numbers = [12, 9, 31, 18, 22]
 const names = ['Joe Schmo', 'Ms. Buckets', 'Harvey Kay', 'Sally Talls', 'MK DiBoux']
 const positions = ['Center', 'Point Guard', 'Shooting Guard', 'Power Forward', 'Small Forward']
-const ponits = {
+const points = {
 	12: [14, 32, 7, 0, 23],
 	9: [19, 0, 11, 22, 0],
 	31: [0, 30, 16, 0, 25],
