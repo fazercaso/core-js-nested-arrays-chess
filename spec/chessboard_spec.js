172dc00f09bafa373df920815707c808e53faae8
kayday <kadaniilo@gmail.com>
2019-05-29 15:39:47 +0300

    Adds renderChessboard

--- spec/chessboard_spec.js
+++ spec/chessboard_spec.js
@@ -1,13 +1,13 @@
-describe("Board", function() {
+describe("Board", function () {
 
   let board;
-  
-  beforeEach(function() {
+
+  beforeEach(function () {
     board = generateChessboard();
   });
 
-  describe("placePiece", function() {
-    it("should return the correct piece for a given starting position", function() {
+  describe("placePiece", function () {
+    it("should return the correct piece for a given starting position", function () {
       expect(board[0][0]).toEqual('Ч Ладья');
       expect(board[1][1]).toEqual('Ч Пешка');
       expect(board[7][7]).toEqual('Б Ладья');
@@ -15,12 +15,22 @@ describe("Board", function() {
     });
   });
 
-  describe("init", function() {
-    it("should generate an 8x8 array", function() {
+  describe("init", function () {
+    it("should generate an 8x8 array", function () {
       expect(board.length).toEqual(8);
       for (let i = 0; i < board.length; i++) {
         expect(board[i].length).toEqual(8);
       }
+    });
+  });
+
+  describe("renderChessboard", function () {
+    it("should return a string", function () {
+      expect(typeof renderChessboard(board)).toEqual('string');
+    });
+
+    it("should have an emoji (first element is `♖`)", function () {
+      expect(renderChessboard(board)[0]).toEqual('♖');
     });
   });
 });
