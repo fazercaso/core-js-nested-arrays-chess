2c4c26ee38e4b3815d331ff2a1b94b90412ba039
Максимилиан Андриянов <fazercaso@gmail.com>
2022-06-07 19:40:43 +0300

    Release 0

--- chessboard.js
+++ chessboard.js
@@ -1,7 +1,17 @@
 function generateChessboard() {
+    const board = [];
     const figures = ['Ладья', 'Конь', 'Слон', 'Ферзь', 'Король', 'Слон', 'Конь', 'Ладья'];
     const pawns = new Array(8).fill('Пешка');
-    
+    const empty = new Array(8).fill(null);
+    const boardLength = 8;
+    for(let i = 0; i < boardLength; i++) {
+        if(i === 0){board.push(figures.map((el) => 'Б ' + el))}
+        else if (i === 1){board.push(pawns.map((el) => 'Б ' + el))}
+        else if (i === 6){board.push(pawns.map((el) => 'Ч ' + el))}
+        else if (i === 7){board.push(figures.map((el) => 'Ч ' + el))}
+        else {board.push(empty)}
+    }
+    return board
     // Тут твой код. Верни массив массивов.
 }
 
@@ -20,8 +30,11 @@ function renderChessboard() {
         'Ч Ладья': '♜',
         'Ч Пешка': '♟'
     }
+       const result = generateChessboard().map((el) => el.map((el) => emojis[el] ? emojis[el] : ' ').join(' ')).join('\n')
        
+       return result
     // Тут твой код.
 }
+console.log(renderChessboard())
 
 module.exports = {generateChessboard, renderChessboard};
